5ee50dc420f0cd4ef49f2787e76e4379b28367aa
tyttiojala <tytti.ojala@gmail.com>
2026-02-15 15:21:28 +0100

    Add files via upload

---
+++ popularityvsfollowers.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+
+df = pd.read_csv("C:/Users/Ik/Desktop/C++/Assignment 1/artist_data.csv")
+
+
+
+# Looking at just correlation between followers and popularity
+corr = df['followers'].corr(df['artist_popularity'])
+print("Correlation between followers and popularity:", corr)
+
+# Scatter plot to vizualize
+plt.figure()
+plt.scatter(df['followers'], df['artist_popularity'], alpha=0.5)
+plt.title('Followers vs Popularity')
+plt.xlabel('Followers')
+plt.ylabel('Popularity')
+plt.show()
+
+
+#Linear regression model
+df = df.dropna(subset=["followers", "artist_popularity"])
+df['followers_log'] = np.log(df['followers'] + 1)
+x = sm.add_constant(df['followers_log'])
+y = df['artist_popularity']
+
+model = sm.OLS(y, x).fit()
+print(model.params)
+
+
+# Plotting the regression line
+plt.figure()
+plt.scatter(df['followers_log'], df['artist_popularity'], alpha=0.5, label='Data Points')
+x_values = np.linspace(df['followers_log'].min(), df['followers_log'].max(), 100)
+y_values = model.params[0] + model.params[1] * x_values
+plt.plot(x_values, y_values)
+plt.title("Regression: Popularity vs log(Followers)")
+plt.xlabel("log(Followers)")
+plt.ylabel("Popularity")
+plt.show()
+
+#Over performers and lgeacy artists
+df['predicted_popularity'] = model.predict(x)
+df['residual'] = df['artist_popularity'] - df['predicted_popularity']
+
+overperformers = df.sort_values("residual", ascending=False).head(10)
+legacy_artists = df.sort_values("residual").head(10)
+
+print("Top 10 overperformers:")
+print(overperformers[["artist_name", "followers", "artist_popularity", "residual"]])
+print("\nTop 10 legacy artists:")
+print(legacy_artists[["artist_name", "followers", "artist_popularity", "residual"]])
+
