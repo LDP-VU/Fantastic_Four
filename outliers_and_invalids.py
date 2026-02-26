85903e40d396defca05ca8b9c5a729b2a960847e
tyttiojala <tytti.ojala@gmail.com>
2026-02-26 14:10:44 +0100

    Update outliers_and_invalids.py

--- outliers_and_invalids.py
+++ outliers_and_invalids.py
@@ -4,6 +4,13 @@ import sqlite3
 
 conn = sqlite3.connect("spotify_database.db")
 
+df = pd.read_sql("""
+SELECT *
+FROM albums_data
+JOIN features_data
+ON albums_data.track_id = features_data.id
+""", conn)
+
 # Start with detecting outliers in different features with IQR method
 feature = "danceability"
 
@@ -16,12 +23,12 @@ outliers = df[(df[feature] < lower) | (df[feature] > upper)]
 print(outliers.head())
 
 # Then outliers in popularity with IQR method
-pop_Q1 = df["popularity"].quantile(0.25)
-pop_Q3 = df["popularity"].quantile(0.75)
+pop_Q1 = df["album_popularity"].quantile(0.25)
+pop_Q3 = df["album_popularity"].quantile(0.75)
 pop_IQR = pop_Q3 - pop_Q1
 
 pop_lower = pop_Q1 - 1.5 * pop_IQR
 pop_upper = pop_Q3 + 1.5 * pop_IQR
-pop_outliers = df[(df["popularity"] < pop_lower) | (df["popularity"] > pop_upper)]
+pop_outliers = df[(df["album_popularity"] < pop_lower) | (df["album_popularity"] > pop_upper)]
 print(pop_outliers.head())
 
