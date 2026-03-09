855b72e782e48db2149c7fc7ba4a1396e9bbf474
tyttiojala <tytti.ojala@gmail.com>
2026-03-09 22:20:23 +0100

    Update feature_and_genre_selection.py

--- part-5/feature_and_genre_selection.py
+++ part-5/feature_and_genre_selection.py
@@ -9,6 +9,9 @@ import os
 # Add the directory to the search path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
 
+# Import earlier functions
+from part-1.Genres_inspection import atop_10_by_genre
+from exercise3.feature_analysis import feature_distribution
 
 # Setting dashboard layout
 st.set_page_config(
@@ -16,3 +19,10 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded"
 )
+
+# Select either genre or feature analysis
+option_type = st.sidebar.selectbox(
+    "Select type of analysis",
+    ["Genre", "Feature"]
+)
+
